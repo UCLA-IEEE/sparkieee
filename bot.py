949d1c0c99509bf3037af5f57f7db72a4f90ec0e
Bryan Wong <bryanjwong@g.ucla.edu>
2020-12-24 15:24:40 -0800

    added basic project directory functionality

--- bot.py
+++ bot.py
@@ -1,14 +1,41 @@
 import creds
 import discord
-from creds import BOT_TOKEN
+from creds import *
 from discord.ext import commands
 
-
-client = commands.Bot(command_prefix='i.')
+client = commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
-    print('SparkIEEE is ready!')
+    print(f'SparkIEEE has logged in as {client.user}')
+
+@client.command()
+async def projects(ctx):
+    msg = 'Here is a list of our currently active projects:\n' \
+          f'```{list(PROJECTS.keys())}```\n' \
+          f'For more info about a particular project, use the `{client.command_prefix}project project_name` command.'
+    await ctx.send(msg)
+
+@client.command()
+async def project(ctx, *args):
+    if len(args) == 0:
+        err_msg = 'Please supply the command with a project argument.\n' \
+                  f'For example, `{client.command_prefix}project OPS` or `{client.command_prefix}project Micromouse`.'
+        await ctx.send(err_msg)
+    elif args[0].upper() not in PROJECTS.keys():
+        err_msg = f'`{args[0]}` is not a valid project.\n' \
+                  'Here is a list of our currently active projects:\n' \
+                  f'```{list(PROJECTS.keys())}```\n'
+        await ctx.send(err_msg)
+    else:
+        info = PROJECTS[args[0].upper()]
+        leads = '\n'.join([f'{name}: @{id}' for name, id in info['LEADS'].items()])
+        msg = f'**{args[0]} Project Leads:**\n' \
+              f'```{leads}```' \
+              f'**{args[0]} Links:**'
+        e = discord.Embed(title=f'{args[0]} Facebook Group',
+                          url=f'{info["FB_GROUP"]}')
+        await ctx.send(msg, embed=e)
 
 client.run(BOT_TOKEN)
 
--- creds.py
+++ creds.py
@@ -1,1 +1,50 @@
 BOT_TOKEN = 'INSERT SECRET BOT TOKEN HERE'
+
+PROJECTS = {
+    'OPS': {
+        'LEADS': {
+            'Ryeder Geyer': 'OPS#4995',
+            'Taylor Chung': 'taylor#9605'
+        },
+        'FB_GROUP': 'https://www.facebook.com/groups/ieeeops'
+    },
+
+    'MICROMOUSE': {
+        'LEADS': {
+            'Bradley Schulz': 'bschulz#0605',
+            'Tyler Price': 'tyler.4p#7755'
+        },
+        'FB_GROUP': 'https://www.facebook.com/groups/910014872738486'
+    },
+
+    'AIRCOPTER': {
+        'LEADS': {
+            'Aaron Kuo': 'arno_aaron#3936',
+            'Eric Tang': 'Eric Tang#5218'
+        },
+        'FB_GROUP': 'https://www.facebook.com/groups/aircopter'
+    },
+
+    'DAV': {
+        'LEADS': {
+            'Brandon Le': 'Brandon Le#2216',
+            'David Kao': 'uh oh its david#7756'
+        },
+        'FB_GROUP': 'https://www.facebook.com/groups/673094760079791'
+    },
+
+    'WRAP': {
+        'LEADS': {
+            'David Baum': 'davidbaum#3617'
+        },
+        'FB_GROUP': 'https://www.facebook.com/groups/2609332906044188'
+    },
+
+    'WORKSHOPS': {
+        'LEADS': {
+            'Jackie Lam': 'kittyslayer#4665',
+            'Travis Graening': 'Travis Graening#7344'
+        },
+        'FB_GROUP': 'https://www.facebook.com/groups/324536415200701'
+    }
+}
