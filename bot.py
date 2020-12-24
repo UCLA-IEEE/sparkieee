9af4f5b00da2266e21bb545b5b65ec94a5f9044e
Bryan Wong <bryanjwong@g.ucla.edu>
2020-12-24 15:57:29 -0800

    implemented help command

--- bot.py
+++ bot.py
@@ -3,11 +3,34 @@ import discord
 from creds import *
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='.', help_command=None)
 
 @client.event
 async def on_ready():
     print(f'SparkIEEE has logged in as {client.user}')
+
+@client.command()
+async def help(ctx):
+    msg = 'Hello! I am **SparkIEEE**, an IEEE Discord bot created by Bryan Wong. \n' \
+          'I can assist you with checking off project completion and more!\n\n' \
+          f'**Commands:**\n' \
+          '```projects          directory of current active projects\n' \
+          'project p         look up project lead info, deadlines, links, and more for p```' \
+          f'To run a command, type in `{client.command_prefix}[command]`.\n\n' \
+          '**Links:**\n' \
+
+    # todo: add image urls/descriptions
+    ieee_website = discord.Embed(title='IEEE at UCLA Website',
+                              url='https://ieeebruins.com/')
+    ieee_linktree = discord.Embed(title='IEEE Linktree',
+                                 url='https://linktr.ee/uclaieee')
+    github = discord.Embed(title='SparkIEEE on Github',
+                              url='https://github.com/bryanjwong/sparkieee')
+
+    await ctx.send(msg)
+    await ctx.send(embed=ieee_website)
+    await ctx.send(embed=ieee_linktree)
+    await ctx.send(embed=github)
 
 @client.command()
 async def projects(ctx):
