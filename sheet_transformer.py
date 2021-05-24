36a2d4ea985301b5bb046e5aff1efc0cdac17757
RogueArt <altoraries@gmail.com>
2021-05-23 19:16:47 -0700

    Removed bot ID; rolled back temporary modifications

--- sheet_transformer.py
+++ sheet_transformer.py
@@ -17,13 +17,13 @@ class SheetTransformer:
             with open('token.pickle', 'rb') as token:
                 creds = pickle.load(token)
         if not creds or not creds.valid:
-            # if creds and creds.expired and creds.refresh_token:
-            #     creds.refresh(Request())
-            # else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
 
